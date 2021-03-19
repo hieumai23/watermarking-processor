fa5b15b9ddf023fbfb9555fd44987c7b452dea97
Tai Duc Nguyen <taiducnguyen.drexel@gmail.com>
2021-03-19 01:28:04 -0400

    CLEAN UP

--- divider_8b_revised/mypack.vhd
+++
@@ -1,9 +0,0 @@
--------------------------------------
--- Defines the dataPath width
---
--------------------------------------
-package mypackage is
-   constant XBITS :INTEGER := 8; 
-   constant YBITS :INTEGER := 8;
-   constant GRAIN :INTEGER := 2; --Allways in 2!!!!
-end mypackage;
